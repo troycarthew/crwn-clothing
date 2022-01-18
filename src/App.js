f5e456b96ab599ce0ba8afe8e14f8f4f2d3161a3
Troy Carthew <tcarthew@web2you.com.au>
2022-01-19 10:19:16 +1100

    broken homepage into smaller components and finished styling the menu component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './hompage.component.jsx';
+import HomePage from './pages/homepage/hompage.component.jsx';
 
 function App() {
   return (
---
+++ src/pages/homepage/hompage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../components/directory/directory.component';
+
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className='hompage'>
+        <Directory/>
+    </div>
+);
+
+export default HomePage;
