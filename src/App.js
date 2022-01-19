f7544714fda017d3459850c4fc6a7dd66679e3c7
Troy Carthew <tcarthew@web2you.com.au>
2022-01-19 13:37:05 +1100

    finished FormInput and custom Button components and applied to signin component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/hompage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-an-sign-up.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
